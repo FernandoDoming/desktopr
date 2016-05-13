c07f675693100ee68a5dff24a35c1ea11b0fad31
Fernando Dominguez <fernan__93@hotmail.com>
2016-05-13 19:41:00 +0200

    Remove dists from git

--- dist/osx/Desktopr.app/Contents/Resources/app/make_dist.sh
+++
@@ -1,3 +0,0 @@
-electron-packager . Desktopr --platform=darwin --arch=x64 --version=0.36.0 --icon=Icon.icns
-electron-packager . Desktopr --platform=win32 --arch=x64 --version=0.36.0 --icon=Icon.icns
-electron-packager . Desktopr --platform=linux --arch=x64 --version=0.36.0 --icon=Icon.icns
