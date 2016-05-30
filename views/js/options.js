cb2971b9b9e5b45c45a09e4619883d318282ae84
Fernando Dominguez <fernan__93@hotmail.com>
2016-05-30 13:08:01 +0200

    Update settings from view

--- views/js/options.js
+++ views/js/options.js
@@ -4,6 +4,7 @@ const $   = require('jquery');
 ipc.on('init-settings', function (sender, settings) {
   $('input[type="checkbox"][data-key="open_gallery"]').attr('checked', settings.open_gallery);
   $('input[type="checkbox"][data-key="allow_nsfw"]').attr('checked', settings.allow_nsfw);
+  $('input[type="radio"][data-key="menu_on_alt"][value="' + settings.menu_on_alt + '"]').prop('checked', true);
 });
 
 ipc.send('request-settings');
@@ -12,5 +13,12 @@ $('input[type="checkbox"]').on('click', function () {
   ipc.send('set-option', {
     key: $(this).data('key'),
     value: $(this).is(':checked'),
+  });
+});
+
+$('input[type="radio"]').on('click', function () {
+  ipc.send('set-option', {
+    key: $(this).data('key'),
+    value: $(this).attr('value') === 'true' ? true : false,
   });
 });
