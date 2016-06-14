e1dfb606906ac6abc5822f8219528e26314f4c08
Fernando Domínguez <fdominguez@redborder.net>
2016-06-14 15:31:10 +0200

    Select sources in settings

--- views/js/options.js
+++ views/js/options.js
@@ -5,20 +5,36 @@ ipc.on('init-settings', function (sender, settings) {
   $('input[type="checkbox"][data-key="open_gallery"]').attr('checked', settings.open_gallery);
   $('input[type="checkbox"][data-key="allow_nsfw"]').attr('checked', settings.allow_nsfw);
   $('input[type="radio"][data-key="menu_on_alt"][value="' + settings.menu_on_alt + '"]').prop('checked', true);
+
+  settings.sources.forEach(function (e) {
+    $(`input[type="checkbox"][data-key="${e}"]`).prop('checked', true);
+  });
 });
 
 ipc.send('request-settings');
 
-$('input[type="checkbox"]').on('click', function () {
+$('#general input[type="checkbox"]').on('click', function () {
   ipc.send('set-option', {
     key: $(this).data('key'),
     value: $(this).is(':checked'),
   });
 });
 
-$('input[type="radio"]').on('click', function () {
+$('#keys input[type="radio"]').on('click', function () {
   ipc.send('set-option', {
     key: $(this).data('key'),
     value: $(this).attr('value') === 'true' ? true : false,
+  });
+});
+
+$('#sources input[type="checkbox"]').on('click', function () {
+  let sources = [];
+  $('#sources input:checked').each(function() {
+    sources.push($(this).data('key'));
+  });
+
+  ipc.send('set-option', {
+    key: 'sources',
+    value: sources,
   });
 });
