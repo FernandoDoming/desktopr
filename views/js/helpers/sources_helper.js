a5106ba716b571ed57a82a3a040ce9c594e2b0d7
Fernando Domínguez <fdominguez@redborder.net>
2016-06-16 15:27:26 +0200

    Add more sources

--- views/js/helpers/sources_helper.js
+++ views/js/helpers/sources_helper.js
@@ -1,5 +1,8 @@
 const HUMANIZATION = {
   EditorsChoice: "Editors' Choice",
+  FreshToday: "Fresh Today",
+  FreshYesterday: "FreshYesterday",
+  FreshWeek: "Fresh this week"
 };
 
 let SourcesHelper = {
