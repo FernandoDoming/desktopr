4d29cb97cad89a27354e517d6668efa36c71b0b2
Fernando Dominguez <fernan__93@hotmail.com>
2016-06-18 13:27:52 +0200

    Different templates for properties

--- views/js/constants.js
+++ views/js/constants.js
@@ -1,12 +1,13 @@
 let CONSTANTS = {
   PHOTOS: {
-    DISPLAYABLE_DETAILS: ['name', 'description', 'user', 'camera',
-        'iso', 'shutter_speed', 'aperture', 'times_viewed', 'rating',
+    IMPORTANT_DETAILS: ['name', 'description', 'user'],
+    PHOTOGRAPHY_DETAILS: ['camera', 'iso', 'shutter_speed', 'aperture'],
+    DISPLAYABLE_DETAILS: ['times_viewed', 'rating',
         'category', 'location', 'latitude', 'longitude', 'taken_at', 'for_sale',
         'width', 'height', 'votes_count', 'favorites_count', 'comments_count',
         'nsfw', 'sales_count', 'for_sale_date', 'highest_rating', 'highest_rating_date',
         'collections_count', 'image_format', 'lens', 'focal_lenght',
-        'positive_votes_count', 'comments']
+        'positive_votes_count', 'comments'],
   },
 };
 
--- views/js/image.js
+++ views/js/image.js
@@ -4,11 +4,13 @@ const API500px = require('500px');
 const api500px = new API500px('9FNw3T1ywcR5PC0LMsTxrsSm6CH47HAYENQvh81L');
 const $ = window.$ = window.jQuery = require('jquery');
 const Handlebars = require('handlebars');
+const Snackbar = require('./../js/snackbar.js');
 const Drawer = require('./../js/drawer.js');
 
 const CONSTANTS = require('./../js/constants.js');
 
 let drawer = null;
+let snackbar = null;
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
@@ -17,12 +19,14 @@ String.prototype.capitalize = function() {
 ipc.on('request-image', function (event, data) {
   api500px.photos.getById( data.id, { image_size: 2048 }, showcase);
 
+  snackbar = new Snackbar();
   drawer = new Drawer({
     title: 'Details',
     action: {
       class: 'fa fa-desktop',
       callback: function () {
         ipc.send('set-background', { id: data.id });
+        snackbar.show(`Setting ${data.name} as the background image`);
       }
     }
   });
@@ -32,29 +36,20 @@ function showcase(error, results) {
   if (error) { return; }
 
   let photo = results.photo;
-  let template = Handlebars.compile( $('#description-template').html() );
 
   document.getElementById('showcase').src = photo.image_url;
 
-  CONSTANTS.PHOTOS.DISPLAYABLE_DETAILS.forEach(function (prop) {
-    switch(prop) {
-      case 'user':
-        $('#drawer').append(template({
-          key: 'Author',
-          value: `${photo.user.firstname} ${photo.user.lastname}`
-        }));
-        break;
-
-      default:
-        if (photo[prop] !== undefined && photo[prop] !== ''
-              && photo[prop] !== null) {
-          let html = template({
-            key: prop.replace(/_/g, ' ').capitalize(),
-            value: photo[prop]
-          });
-
-          $('#drawer').append(html);
+  for (let key of Object.keys(CONSTANTS.PHOTOS)) {
+    let entries = CONSTANTS.PHOTOS[key].map(function (prop) {
+      return {
+        key: prop.capitalize(),
+        value: photo[prop]
       }
-    }
-  });
+    });
+
+    let template = Handlebars.compile( $(`#${key.toLowerCase()}_template`).html() )
+    $('#drawer').append(template({
+      entries: entries
+    }));
+  }
 }
